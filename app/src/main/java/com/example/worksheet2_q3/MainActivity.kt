27dc7ac63c96ad764a5c1a6ed0971bc4120b18b3
Efim Sokolov <efim@bu.edu>
2024-09-17 17:31:39 -0400

    Trimmed unused import

--- app/src/main/java/com/example/worksheet2_q3/MainActivity.kt
+++ app/src/main/java/com/example/worksheet2_q3/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.worksheet2_q3
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
